7c9d10b437b0cd03f7c1d0a8574ce9510d72162f
Drakee <thanhlong9123@gmail.com>
2024-11-01 00:23:22 +0700

    feat: implement post features

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { ROLE } from 'src/auth/util/@enum/role.enum';
+import { Roles } from 'src/auth/@decorator/roles.decorator';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Roles(ROLE.ADMIN, ROLE.USER)
+  @Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(createPostDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(id, updatePostDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postsService.remove(id);
+  }
+}
---
+++ src/posts/posts.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostsController } from './posts.controller';
+import { PrismaService } from 'src/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import access_tokenJwtConfig from 'src/auth/@config/access_token-jwt.config';
+import refresh_tokenJwtConfig from 'src/auth/@config/refresh_token-jwt.config';
+import { AuthService } from 'src/auth/auth.service';
+import { UsersService } from 'src/users/users.service';
+import { UserRepository } from 'src/users/users.repository';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync(access_tokenJwtConfig.asProvider()),
+    ConfigModule.forFeature(access_tokenJwtConfig),
+    ConfigModule.forFeature(refresh_tokenJwtConfig),
+  ],
+  controllers: [PostsController],
+  providers: [
+    PostsService,
+    PrismaService,
+    AuthService,
+    JwtService,
+    UsersService,
+    UserRepository,
+  ],
+})
+export class PostsModule {}
