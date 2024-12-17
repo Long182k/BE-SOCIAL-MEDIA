4c46236a5bc9a76e8cab6d9b4f82d033ed626c30
Drakee <thanhlong9123@gmail.com>
2024-12-18 02:14:05 +0700

    add: public decorator for check health

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Public } from 'src/auth/@decorator/public';
 
+@Public()
 @Controller('health')
 export class HealthController {
   @Get()
   check() {
     return { status: 'OK' };
   }
-} 
+}
